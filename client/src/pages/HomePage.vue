502257aa05511e218f7fdeb82f3c270cd0ab046d
Laura <lauraalspaugh2002@icloud.com>
2024-03-01 15:16:46 -0700

    need to adjust nav

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,8 +1,25 @@
 <template>
   <div class="container-fluid">
-    <section class="row">
-      <div class="col-12 equipment-site">
-
+    <section class="row equipment-site">
+      <div class="col-12  fs-3 text-center text-white p-4">
+        <p class="p-0">Golden Rule</p>
+        <p class="p-0">Equipment</p>
+      </div>
+      <div class="col-12 mt-5 d-flex justify-content-evenly align-items-center text-light p-4 green-section">
+        <p>Home</p>
+        <p>Machinery</p>
+        <p>Dismantled Machinery</p>
+        <p>Attachments</p>
+        <p>Trucks and Trailers</p>
+        <p>Parts</p>
+        <p>Financing</p>
+        <p>Contact Us</p>
+        <p>Store</p>
+      </div>
+      <div class="col-12 text-white fs-1 ms-5 pt-0 ">
+        <p>TIRED of Paying Exhorbitant Prices</p>
+        <p>for Forestry Parts?</p>
+        <button class="btn contact-button">CONTACT US</button>
       </div>
     </section>
   </div>
@@ -24,5 +41,17 @@ export default {
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
+  height: 580px;
+}
+
+.green-section {
+  background-color: #145A32;
+  height: 85px;
+}
+
+.contact-button {
+  background-color: #F1C40F;
+  border-radius: 0;
+  color: gray;
 }
 </style>
